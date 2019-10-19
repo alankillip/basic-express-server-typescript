f56fe87c21ad27b2b7eb0eed6f985fde7856612d
alankillip <alan.kilip@dentsuaegis.com>
2019-10-19 19:41:52 +0100

    Take port from env

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Instantiate the server
 const app = express();
