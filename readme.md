ce4978bf728f0edc1bb5660f4af866fe3e48e9d4
alankillip <alan.kilip@dentsuaegis.com>
2019-10-19 19:00:28 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+Basic Typescript Express Server
+
+npm install
+npm run start
+
+app served on localhost:3000
+
+When files change the typescript is transpiled to javascript and sent to the build folder, then nodemon restarts the server
+Browser must be refreshed manually.
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+// Instantiate the server
+const app = express();
+
+// Define a folder for static files
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+
+
+// Basic route ( get )
+app.get('/', (req, res) => {
+    res.send('hello.');
+});
+
+// Basic route ( post )
+app.post('/post', (req, res) => {
+    console.info('request body received', req.body);
+    res.send('ok');
+});
+
+
+// Tell express to listen on port
+app.listen(port, err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
+
